09c620f232f7c735f86bdfa5ad08e5a38c1b2ea3
百小僧 <monksoul@outlook.com>
2024-11-28 09:52:22 +0800

    😊 发布 `v4.9.6.2` 版本，改进新远程请求 `HttpRemoteOptions` 设计

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.6.1";
+$FurTools = "Furion Tools v4.9.6.2";
 
 # 输出信息
 $copyright = @"
