757f9da1fa1c3d610ad0bb4fffafcd6b01373af5
百小僧 <monksoul@outlook.com>
2024-12-02 20:10:36 +0800

    😊 发布 `v4.9.6.9` 版本，改进 `HTTP` 远程请求分析工具性能

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.6.8";
+$FurTools = "Furion Tools v4.9.6.9";
 
 # 输出信息
 $copyright = @"
