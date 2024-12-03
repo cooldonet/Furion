3300f3e058680c21adf0eea42a08ab91438273a8
百小僧 <monksoul@outlook.com>
2024-12-03 17:20:13 +0800

    😊 发布 `v4.9.6.10` 版本，改进 `HTTP` 远程请求 `RateLimitedStream` 带应用速率限制的流

--- tools/cli.ps1
+++ tools/cli.ps1
@@ -96,7 +96,7 @@ function AddXmlCommentsToProperties($content, $commentsDictionary) {
     return $modifiedContent
 }
 
-$FurTools = "Furion Tools v4.9.6.9";
+$FurTools = "Furion Tools v4.9.6.10";
 
 # 输出信息
 $copyright = @"
